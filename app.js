691a51d6c69ec02327321660692869a8c4ddde4e
wrenr <wren@hancocklimited.com>
2014-02-23 00:00:29 -0800

    add entire game framework on server side

--- app.js
+++ app.js
@@ -44,7 +44,14 @@ server = http.createServer(app).listen(app.get('port'), function(){
 var io = require('socket.io').listen(server);
 var gameClients = new Array();
 var maxClients = 3;
+
+var announceDuration = 3 * 1000; // units = milliseconds, 3 seconds
+
+var gameDuration = 10 * 1000; // units = milliseconds, 3 seconds
+
 app.locals.lobby_size = gameClients.length;
+
+app.locals.max_lobby = maxClients;
 
 
 io.sockets.on('connection', function (socket) {
@@ -70,19 +77,66 @@ io.sockets.on('connection', function (socket) {
     }
 
     userExists = _.indexOf(gameClients, my_id) !== -1;
-    if (userExists) {
-      socket.emit('notification', {text:'You are in the game!'});
-    } else if (gameClients.length < maxClients) {
-      socket.emit('create_user', {id: my_id });
-      gameClients.push(my_id);
-      socket.emit('lobby_size', {size: gameClients.length});
-      app.locals.lobby_size = gameClients.length;
+    if (!userExists) {
+      if (gameClients.length < maxClients) {
+        socket.emit('create_user', {id: my_id });
+        gameClients.push(my_id);
+        socket.emit('lobby_size', {size: gameClients.length});
+        app.locals.lobby_size = gameClients.length;
+
+        //Lobby is now full, start the game!
+        if (gameClients.length == maxClients) {
+          announceGame(socket);
+        }
+      } else {
+        socket.emit('notification', {text:'Game Full!'});
+      }
     } else {
-      socket.emit('notification', {text:'Game Full!'});
+      socket.emit('notification', {text:'You are already in the game!'});
     }
 
    
   });
+
+  announceGame = function(socket) {
+    console.log('announceGame');
+    //notify all clients that game is starting
+    socket.broadcast.emit('game_announce');
+    socket.emit('game_announce');
+    //begin countdown!
+    setTimeout(startGame, announceDuration, socket);
+  };
+
+  startGame = function(socket) {
+    console.log('startGame');
+    //game starts
+    //enable accepting of button requests.
+    socket.broadcast.emit('game_start');
+    socket.emit('game_start');
+    setTimeout(endGame, gameDuration, socket);
+  }
+
+  endGame = function(socket) {
+    console.log('endGame');
+    //game ends
+
+    socket.broadcast.emit('game_end');
+    socket.emit('game_end');
+    //disable accepting of button requests.
+    processResults(socket);
+  }
+
+  processResults = function (socket) {
+    console.log('processResults');
+
+    //compile all user submissions
+    //choose a winner
+    //send results data to all players
+
+    socket.broadcast.emit('game_results', {});
+    socket.emit('game_results', {});
+
+  }
 
 });
 
